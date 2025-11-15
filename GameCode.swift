67ffe0f806f9fc712f20fd2f07f03f9a288c0fca
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-15 14:34:19 +0200

    some refactoring

--- GameCode.swift
+++ GameCode.swift
@@ -9,7 +9,7 @@ at the top level of the file (outside any function). You can't write any other k
 for example if statements and for loops, at the top level; they have to be written inside
 of a function.
 */
-let circle = OvalShape(width: 40, height: 40)
+let ball = OvalShape(width: 40, height: 40)
 let barrierWidth = 300.0
 let barrierHeight = 25.0
 
@@ -19,23 +19,35 @@ let barrier = PolygonShape(points: barrierPoints)
 let funnelPoints = [Point(x: 0, y: 50), Point(x: 80, y: 50), Point(x: 60, y: 0), Point(x: 20, y: 0)]
 let funnel = PolygonShape(points: funnelPoints)
 
-func setup() {
-    circle.position = Point(x: 250, y: 400)
-    scene.add(circle)
-    circle.hasPhysics = true
-    circle.fillColor = .blue
+fileprivate func setupBall() {
+    ball.position = Point(x: 250, y: 400)
+    scene.add(ball)
+    ball.hasPhysics = true
+    ball.fillColor = .blue
+}
 
+fileprivate func setupBarrier() {
     barrier.position = Point(x: 200, y: 150)
     barrier.hasPhysics = true
     scene.add(barrier)
     barrier.isImmobile = true
+}
 
+fileprivate func setupFunnel() {
     funnel.position = Point(x: 200, y: scene.height - 25)
     scene.add(funnel)
     funnel.onTapped = dropBall
     funnel.fillColor = .gray
 }
 
+func setup() {
+    setupBall()
+
+    setupBarrier()
+
+    setupFunnel()
+}
+
 func dropBall() {
-    circle.position = funnel.position
+    ball.position = funnel.position
 }
