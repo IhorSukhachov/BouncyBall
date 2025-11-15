bcecd395574dfd0476398b63cd736cd6a3bca85a
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-15 14:18:08 +0200

    added barrier and physics

--- GameCode.swift
+++ GameCode.swift
@@ -10,7 +10,17 @@ for example if statements and for loops, at the top level; they have to be writt
 of a function.
 */
 let circle = OvalShape(width: 150, height: 150)
+let barrierWidth = 300.0
+let barrierHeight = 25.0
+
+let barrierPoints = [Point(x:0, y: 0), Point(x: 0, y: barrierHeight), Point(x: barrierWidth, y: barrierHeight), Point(x: barrierWidth, y: 0)]
+let barrier = PolygonShape(points: barrierPoints)
+
 func setup() {
     circle.position = Point(x: 250, y: 400)
     scene.add(circle)
+    circle.hasPhysics = true
+    barrier.position = Point(x: 200, y: 150)
+    barrier.hasPhysics = true
+    scene.add(barrier)
 }
