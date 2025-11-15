a9b6f20bddbb496f8d5a6384b669d66f5d19d841
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-15 14:24:07 +0200

    adding funnel

--- GameCode.swift
+++ GameCode.swift
@@ -16,6 +16,9 @@ let barrierHeight = 25.0
 let barrierPoints = [Point(x:0, y: 0), Point(x: 0, y: barrierHeight), Point(x: barrierWidth, y: barrierHeight), Point(x: barrierWidth, y: 0)]
 let barrier = PolygonShape(points: barrierPoints)
 
+let funnelPoints = [Point(x: 0, y: 50), Point(x: 80, y: 50), Point(x: 60, y: 0), Point(x: 20, y: 0)]
+let funnel = PolygonShape(points: funnelPoints)
+
 func setup() {
     circle.position = Point(x: 250, y: 400)
     scene.add(circle)
@@ -23,4 +26,7 @@ func setup() {
     barrier.position = Point(x: 200, y: 150)
     barrier.hasPhysics = true
     scene.add(barrier)
+    barrier.isImmobile = true
+    funnel.position = Point(x: 200, y: scene.height - 25)
+    scene.add(funnel)
 }
